aa06fa2d2222be7eb1d18afc759521473aa6b81f
oscar <oscar.moreno@cti.espol.edu.ec>
2024-08-21 01:19:30 -0500

    Backend deleteing POST across clients

---
+++ graphql/resolvers.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+const User = require("../models/user");
+
+module.exports = {
+  createUser: async function ({ userInput }, req) {
+    //const email = args.userInput.email;
+    // si no uso async debo retornar la promesa
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Email is invalid." });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "password to short!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User already exist!");
+      throw error;
+    }
+
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+};
